61f490c335740c2b4ee11a8a3618a0f7e367c0d8
Fedot Sereoja <sereoja.fedot@stellarchain.io>
2026-02-13 05:06:40 +0200

    add new soroban endpoint

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,7 +1,10 @@
 import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "REMOVED_SENTRY_DSN",
+  integrations: [Sentry.replayIntegration()],
   tracesSampleRate: 1,
   enableLogs: true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
   sendDefaultPii: true,
 });
