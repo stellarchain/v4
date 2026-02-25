f516e1a07a825d18932381bde54b10397f0e4958
Fedot Sereoja <sereoja.fedot@stellarchain.io>
2026-02-26 01:09:01 +0200

    add testnet and futurenet

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -5,7 +5,17 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 if (!isDevelopment) {
   Sentry.init({
     dsn: 'REMOVED_SENTRY_DSN',
-    integrations: [Sentry.replayIntegration()],
+    integrations: [
+      Sentry.replayIntegration(),
+      Sentry.feedbackIntegration({
+        colorScheme: 'system',
+        showBranding: false,
+        triggerLabel: "",
+        formTitle: "Feedback",
+        submitButtonLabel: "Send feedback",
+        messagePlaceholder: "Tell us what did you observe and what we can improve.",
+      }),
+    ],
     tracesSampleRate: 1,
     enableLogs: true,
     replaysSessionSampleRate: 1.0,
